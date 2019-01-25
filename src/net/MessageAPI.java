8eb598bfd79719eb23f200ccf261bb0221d0f5a0
Austin Turner <32046329+austinjturner@users.noreply.github.com>
2019-01-25 13:04:06 -0500

    Remove broken package

--- src/net/MessageAPI.java
+++
@@ -1,8 +0,0 @@
-package net;
-
-public class MessageAPI {
-	public static final int MSG_EMPTY_RESPONSE					= 0;
-	public static final int MSG_BOOTSTRAP_ELEVATOR_SUBSYSTEM 	= 1;
-	public static final int MSG_BOOTSTRAP_FLOOR_SUBSYSTEM 		= 2;
-	public static final int MSG_ITER0_TEST						= 3;
-}
