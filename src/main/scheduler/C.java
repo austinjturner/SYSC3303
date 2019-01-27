33f6b703ffa3801d6bedbcb111eb1395bf471adf
nichowes <nichowes@cmail.carleton.ca>
2019-01-27 18:24:35 -0500

    Added 5 classes
    Added the FSM classes, they haven't been finished however I'll be working on them over the week as well as tonight.

---
+++ src/main/scheduler/C.java
@@ -0,0 +1,10 @@
+package src.main.scheduler;
+
+public class C extends State{
+	
+	public void on() {
+		System.out.println("C + on  = B");
+		// TO-DO Needs proper functionality when overriding State
+	}
+
+}
