55114d5dba9e8f031235cd61415c09597bc02cf4
nichowes <nichowes@cmail.carleton.ca>
2019-02-02 18:30:43 -0500

    Update TestScheduler.java

--- src/main/scheduler/TestScheduler.java
+++ src/main/scheduler/TestScheduler.java
@@ -12,28 +12,28 @@ import src.main.net.*;
  */
 public class TestScheduler {
 	
-	int elevatorID = 666;
-	int targetFloor1 = 10;
-	int targetFloor2 = 5;
-	int targetFloor3 = 2;
-	int targetFloor4 = 3;
-	int targetFloor5 = 7;
-	int targetFloor6 = 8;
+	public static int elevatorID = 666;
+	public static int targetFloor1 = 10;
+	public static int targetFloor2 = 5;
+	public static int targetFloor3 = 2;
+	
+	
 	
 	
 	/* 
 	 * Test to ensure that the floorQueue is initially empty
 	 */
 	@Test
-	public void emptyQueue_testWaitingState(StateMachine machine) {
+	public static void emptyQueue_testWaitingState(StateMachine machine) {
 		assertTrue(machine.floorQueue.isEmpty());
 	}
 	
 	/*
 	 * Test to show overall state sequence, note transition states shown via embedded print statements not assertEquals().
+	 * Simulates someone getting picked up at a floor and pressing a button in the elevator then going to that floor.
 	 */
 	@Test
-	public void transition_testOverallStateTransition(StateMachine machine) {
+	public static void transition_testOverallStateTransition(StateMachine machine) {
 		
 		machine.floorQueue.add(new Destination(targetFloor1, Destination.DestinationType.PICKUP));
 		
@@ -53,9 +53,22 @@ public class TestScheduler {
 		
 	}
 	
-	
-	
-
+	/*
+	 * Test to show the MotorStoppedState goes to the WaitingState when destinationType is wait.
+	 */
+	@Test 
+	public static void motorStoppedState_testNextState(StateMachine machine) {
+		
+		machine.floorQueue.add(new Destination(targetFloor3, Destination.DestinationType.WAIT));
+		machine.enqueueFloorEvent();
+		
+		for (int i = 2; i <= targetFloor3; i++) {
+			machine.elevatorReachedFloorEvent(i);
+		}
+		
+		assertEquals(machine.getState().getClass(), WaitingState.class);
+		
+	}
 	
 	
 	
@@ -64,9 +77,13 @@ public class TestScheduler {
 	public static void main(String[] args) {
 		SchedulerSubsystem ss = new SchedulerSubsystem(new MockRequester(), new Responder());
 		ss.start();
-		
-
 		StateMachine fsm  = ss.getStateMachine();
+		
+		emptyQueue_testWaitingState(fsm);
+		
+		transition_testOverallStateTransition(fsm);
+		
+		motorStoppedState_testNextState(fsm);
 		
 	}
 	
