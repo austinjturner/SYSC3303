47fee675a70696ee3e0994985fcd780fcf4b95f7
nichowes <nichowes@cmail.carleton.ca>
2019-03-09 15:50:24 -0500

    Update DoorClosedState.java

--- src/main/scheduler/states/DoorClosedState.java
+++ src/main/scheduler/states/DoorClosedState.java
@@ -1,6 +1,8 @@
 package src.main.scheduler.states;
 
+import src.main.net.MessageAPI;
 import src.main.scheduler.StateMachine;
+import src.main.settings.Settings;
 
 /**
  * State representing the elevator with door closed.
@@ -17,18 +19,32 @@ class DoorClosedState extends State {
 	 */
 	public DoorClosedState(StateMachine stateMachine) {
 		super(stateMachine);
+		
+		startDoorWaitTimer();
 	}
-	// Note changed to floorQueue.isEmpty(), doesn't change functionality.
+	
+	private void startDoorWaitTimer() {
+		new DoorWaitTimer(this.stateMachine, Settings.WAIT_TIME_OPEN_DOOR_AND_EXIT_ELEVATOR).start();
+	}
 
 	/**
 	 * Conditional check for the following state transition.
 	 * 
 	 * @return State Returns next state for the elevator.
 	 */
-	@Override
-	public State defaultEvent() {
+	public State doorTimerEvent() {
 		String s = new String(getStateName());
 		this.stateMachine.schedulerSubsystem.debug("This is transition state: " + s);
+		
+		while (!(this.stateMachine.schedulerSubsystem.areElevatorDoorsClosed(this.stateMachine.elevatorID))) {
+			if (this.stateMachine.faultMessage == null) {
+				this.stateMachine.setFault(MessageAPI.FaultType.ElevatorFailedToCloseDoors);
+				this.stateMachine.schedulerSubsystem.sendOpenDoorMessage(
+						this.stateMachine.elevatorID);
+			} else {
+				// keep looping until our doors are closed
+			}
+		} 
 		
 		if (this.stateMachine.floorQueue.isEmpty()) {
 			return new WaitingState(this.stateMachine);
