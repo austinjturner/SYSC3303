93e5cd38bcf605d3bb36630629d5e6c910f716a8
NikolaErak <NikolaErak@dhcp-139-90.cu-wireless-students.carleton.ca>
2019-03-22 13:09:02 -0400

    minor changes

--- src/main/gui/ElevatorView.java
+++ src/main/gui/ElevatorView.java
@@ -14,7 +14,7 @@ import javax.swing.WindowConstants;
 
 public class ElevatorView extends JFrame {
 
-	private JButton[][] elevators;
+	private JButton[][] elevatorSubsystem;
 	private JMenuBar bar;
 	private JMenu menu;
 	private JMenuItem reset;
@@ -24,11 +24,11 @@ public class ElevatorView extends JFrame {
 	public ElevatorView() {
 
 		super("Elevator Subsystem");
-		elevators = new JButton[22][4];
+		elevatorSubsystem = new JButton[22][4];
 
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setSize(1000, 1000);
-		this.setLayout(new GridLayout(22,8));
+		this.setLayout(new GridLayout(22,4));
 
 		//add menu bar and menu items
 		bar = new JMenuBar();
@@ -40,14 +40,11 @@ public class ElevatorView extends JFrame {
 		exit = new JMenuItem("Exit");
 		menu.add(exit);
 
-		//add buttons to frame
-		for(int i = 21; i>=0; i--){
-			for(int j = 0; j<4; j++){
-				elevators[i][j] = new JButton(String.format("%d", i+1));
-				add(elevators[i][j]);
-			}
+		//add direction arrows for floors
+		for(int a = 21; a>=0; a--){
+			elevatorSubsystem[a][0] = new JButton(String.format("%d", a+1));
+			add(elevatorSubsystem[a][0]);
 		}
-
 
 		this.setVisible(true);
 	}
